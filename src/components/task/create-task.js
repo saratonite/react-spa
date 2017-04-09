7185f07701c445fd884dad3e980cd44ae3f4c039
Sarath <sarath.hacks@gmail.com>
2017-04-09 18:25:28 +0530

    Added simple form validation

--- src/components/task/create-task.js
+++ src/components/task/create-task.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Validator from 'validator';
+import _ from 'underscore';
+import classNames from 'classnames';
 
 import { createTask } from '../../actions/task-actions';
 
@@ -12,7 +15,7 @@ class CreateTask extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.state = props.task;
+    this.state = Object.assign({errors:{}},props.task);
 
 
   }
@@ -23,10 +26,38 @@ class CreateTask extends Component {
 
   handleSubmit(){
 
-    console.info("Just clicked submit button")
+    if(!this.validate()){
+      return false;
+    }
+
     this.props.addTask(this.props.projectId,this.state);
 
     this.setState({title:'',description:''});
+
+  }
+
+  /* Vaslidate inputs */
+  validate() {
+
+    let errors = {};
+
+    if(Validator.isEmpty(this.state.title)){
+
+      errors.title = "This field required"
+
+    }
+
+    if(Validator.isEmpty(this.state.description)) {
+      errors.description = "This field required"
+    }
+
+    this.setState({errors:errors});
+
+    if(_.size(errors)){
+      return false;
+    }
+
+    return true;
 
   }
 
@@ -40,17 +71,20 @@ class CreateTask extends Component {
 
   render(){
 
+    let { errors } = this.state;
 
     return(
       <div className="ui segment form">
       <h3>Add Task</h3>
-      <div className="field">
+      <div className={ classNames("field",{'error':errors.title}) }>
         <label>Title</label>
         <input type="text" name="title" onChange={this.handleChange} value={this.state.title} />
+        { errors.title && <div className="ui pointing red basic label">{ errors.title }</div>}
       </div>
-        <div className="field">
+        <div className={ classNames("field",{'error':errors.description}) }>
           <label>Description</label>
           <textarea rows="2" name="description" onChange={this.handleChange} value={this.state.description}></textarea>
+          { errors.description && <div className="ui pointing red basic label">{ errors.description }</div>}
         </div>
 
         <div className="field">
